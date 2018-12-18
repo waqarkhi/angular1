2d90230904b6eeda459c2d750c27550c1355bdd3
waqarkhi <45649398+waqarkhi@users.noreply.github.com>
2018-12-18 11:03:55 +0500

    Update index.php

--- index.php
+++ index.php
@@ -199,22 +199,6 @@
 <script type="text/javascript" src="js/info.js"></script>
 <script type="text/javascript" src="js/wixe.js"></script>
 <script src="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js"></script>
-<!-- Start of Tawk to Script-->
-<script type="text/javascript">
-  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-  (function(){
-    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-    s1.async=true;
-    s1.src='https://embed.tawk.to/599447791b1bed47ceb04eff/default';
-    s1.charset='UTF-8';
-    s1.setAttribute('crossorigin','*');
-    s0.parentNode.insertBefore(s1,s0);
-  })();
-</script>
-<!--End of Tawk.to Script-->
-
-
-
 
 </body>
 </html>
